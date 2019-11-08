fce5307e4a56e82933e1e3f8957d52ce4fbd6419
garrett <garrett>
2019-11-08 14:14:50 +0800

    change 1

--- 03_循环/hm_05_偶数求和.py
+++ 03_循环/hm_05_偶数求和.py
@@ -8,8 +8,11 @@ i = 0
 while i <= 100:
 
     # 判断变量i中的数值，是否是一个偶数
+    # 偶数 i % 2 == 0
+    # 奇数 i % 2 != 0
     if i % 2 == 0:
         print(i)
-    result += i
+        result += i
+
     i += 1
 print("0-100之间所有偶数的累计求和为=%d" % result)
