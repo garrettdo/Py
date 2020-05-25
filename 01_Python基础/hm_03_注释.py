26047702fd38fdde90aec8ff1c7d0dc6c7a675fe
garrett <garrettdo1995@gmail.com>
2020-05-25 15:13:28 +0800

    update even

--- 01_Python基础/hm_03_注释.py
+++ 01_Python基础/hm_03_注释.py
@@ -3,11 +3,13 @@ print("hello hello")
 
 # 这是第二个注释
 print("hello world")
-
+'''
 print("00")
 print("01")
 print("02")
 print("03")
 print("04")
-
-print("hello world") # 这是第三个注释
+'''
+# print("hello world") # 这是第三个注释
+# print("hello world") # 这是第三个注释
+# print("hello world") # 这是第三个注释
